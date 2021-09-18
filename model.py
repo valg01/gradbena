c17a417798d1c65b91dc11af3ceb209b97749a6c
Val Groleger <val.groleger@gmail.com>
2021-09-18 12:50:24 +0200

    commit on main

--- model.py
+++ model.py
@@ -5,9 +5,18 @@ class Hisa:
         self.ime = ime
         self.proracun = proracun
         self.prostori = []
+        self.aktualni_prostor = None
     
     def dodaj_prostor(self, prostor):
         self.prostori.append(prostor)
+        if not self.aktualni_prostor:
+            self.aktualni_prostor = prostor
+
+    def odstrani_prostor(self, prostor):
+        self.prostori.remove(prostor)
+
+    def zamenjaj_spisek(self, prostor):
+        self.aktualni_prostor = prostor
 
     def stroski(self):
         strosek = 0
@@ -31,7 +40,7 @@ class Hisa:
         opravljeni = self.stevilo_opravljenih()
         vsa_dela = 0
         for prostor in self.prostori:
-            vsa_dela += len(prostor.dela)
+            vsa_dela += prostor.stevilo_del()
         return round(opravljeni / vsa_dela, 2)
     
     def delez_neopravljenih(self):
@@ -42,6 +51,18 @@ class Hisa:
         for prostor in self.prostori:
             st += prostor.stevilo_zamujenih()
         return st
+    
+    def skupno_stevilo_neopravljenih(self):
+        st = 0
+        for prostor in self.prostori:
+            st += prostor.stevilo_neopravljenih()
+        return st
+    
+    def dodaj_delo(self, delo):
+        self.aktualni_prostor.dodaj_delo(delo)
+    
+    def odstrani_delo(self, delo):
+        self.aktualni_prostor.odstrani_delo(delo)
 
 class Prostor:
     def __init__(self, ime):
@@ -51,18 +72,32 @@ class Prostor:
     def dodaj_delo(self, delo):
         self.dela.append(delo)
     
+    def odstrani_delo(self, delo):
+        self.dela.remove(delo)
+    
     def stevilo_zamujenih(self):
         stevilo = 0
         for delo in self.dela:
             if delo.zamuja():
                 stevilo += 1
         return stevilo
+    
+    def stevilo_neopravljenih(self):
+        stevilo = 0
+        for delo in self.dela:
+            if not delo.opravljeno():
+                stevilo += 1
+        return stevilo
+    
+    def stevilo_del(self):
+        return len(self.dela)
 
     def strosek_prostora(self):
         skupna_cena = 0
         for delo in self.dela:
             skupna_cena += delo.cena
         return skupna_cena 
+    
     
 class Delo:
     def __init__(self, ime, opis, tezavnost, cena, material = None, rok = None):
