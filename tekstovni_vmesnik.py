fe878ef292333e2eaaea859c5d19104c75497534
Val Groleger <val.groleger@gmail.com>
2021-09-30 14:51:57 +0200

    json

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,17 +1,23 @@
 from typing import ValuesView
 from model import Hisa, Prostor, Delo
+import json
 
-model = Hisa()
+IME_DATOTEKE = "stanje.json"
+try:
+    moj_model = Hisa.preberi_iz_dat(IME_DATOTEKE)
+except:
+    moj_model = Hisa("")
 
-DODAJ_PROSTOR = 1
-POBRISI_PROSTOR = 2
-ZAMENJAJ_PROSTOR = 3
-DODAJ_PRORACUN = 4
-DODAJ_STROSEK = 5
-DODAJ_DELO = 6
-POBRISI_DELO = 7
-OPRAVI_DELO = 8
-IZHOD = 9
+PREIMENUJ_HISO = 1
+DODAJ_PROSTOR = 2
+POBRISI_PROSTOR = 3
+ZAMENJAJ_PROSTOR = 4
+DODAJ_PRORACUN = 5
+DODAJ_STROSEK = 6
+DODAJ_DELO = 7
+POBRISI_DELO = 8
+OPRAVI_DELO = 9
+IZHOD = 10
 
 def preberi_stevilo():
     while True:
@@ -38,24 +44,24 @@ def prikaz_prostora(prostor):
     neopravljena_dela = prostor.stevilo_neopravljenih()
     strosek = prostor.strosek_prostora()
     if zamujena_dela:
-        return f"Za {prostor.ime} ste porabili {strosek}. Vseh del je {dela}, od tega jih je {neopravljena_dela} neopravljenih, {zamujena_dela} pa jih zamuja."
+        return f"Za {prostor.ime} ste porabili {strosek}. Vseh del je {dela}, od tega jih je {neopravljena_dela} neopravljenih, {zamujena_dela} pa jih zamuja. Za prostor potrebujete {prostor.mnozica_materialov()}."
     elif not zamujena_dela and neopravljena_dela:
-        return f"Za {prostor.ime} ste porabili {strosek}. Vseh del je {dela}, od tega jih je {neopravljena_dela} neopravljenih"
+        return f"Za {prostor.ime} ste porabili {strosek}. Vseh del je {dela}, od tega jih je {neopravljena_dela} neopravljenih. Za prostor potrebujete {prostor.mnozica_materialov()}."
     else:
         return f"Za {prostor.ime} ste porabili {strosek}. Super! Opravili ste že vseh {dela} del."
 
 def prikaz_dela(delo):
     if delo.zamuja() and not delo.opravljeno:
-        return f"{delo.tezavnost} delo: {delo} je neopravljeno in zamuja!!!!"
+        return f"{delo.tezavnost} delo: {delo} je neopravljeno in zamuja!!!! Za delo potrebujete {delo.material}, ocenjen strošek je {delo.cena} EUR."
     elif not delo.opravljeno:
-        return f"{delo.tezavnost} delo: {delo} je neopravljeno"
+        return f"{delo.tezavnost} delo: {delo} je neopravljeno! Za delo potrebujete {delo.material}, ocenjen strošek je {delo.cena} EUR."
     else:
-        return f"{delo.tezavnost} delo: {delo} je opravljeno"
+        return f"{delo.tezavnost} delo: {delo} je opravljeno. Porabili ste {delo.cena} EUR"
 
 def izberi_prostor(model):
     return izberi_moznost([(prostor, prikaz_prostora(prostor)) for prostor in model.prostori])
 
-def izberi_opravilo(model):
+def izberi_delo(model):
     return izberi_moznost(
         [
             (delo, prikaz_dela(delo))
