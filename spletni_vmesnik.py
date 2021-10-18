869f7ee1c64b02f97162865a865d5ed47a045140
Val Groleger <val.groleger@gmail.com>
2021-10-18 21:00:50 +0200

    dodaj prostor

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -10,11 +10,25 @@ except FileNotFoundError:
 @bottle.get("/")
 def osnovna_stran():
     return bottle.template(
-        "osnovna_stran.tpl",
+        "osnovna_stran.html",
         neopravljena=moj_model.skupno_stevilo_neopravljenih(),
         zamujena=moj_model.skupno_stevilo_zamujenih(),
         kretenizem=moj_model.aktualni_prostor.dela,
         )
+
+@bottle.get("/dodaj-prostor/")
+def dodaj_prostor_get():
+    return bottle.template(
+        "dodaj_prostor.html"
+    )
+
+@bottle.post("/dodaj-prostor/")
+def dodaj_prostor_post():
+    ime = bottle.request.forms.getunicode("ime")
+    nov_prostor = Prostor(ime)
+    moj_model.dodaj_prostor(nov_prostor)
+    moj_model.shrani_v_dat(IME_DATOTEKE)
+    bottle.redirect("/")
 
 @bottle.post("/dodaj/")
 def dodaj_delo():
@@ -30,7 +44,7 @@ def dodaj_delo():
     bottle.redirect("/")
 
 @bottle.post("/opravi/")
-def opravi_delo():
+def spremeni_opravljeno():
     indeks = bottle.request.forms.getunicode("indeks")
     delo = moj_model.aktualni_prostor.dela[int(indeks)]
     delo.spremeni_opravljeno()
